f8331fb1284e701d65792d4a02935d8e29ddd552
Daniel Leavitt <daniel.leavitt@gmail.com>
2011-10-29 00:59:33 -0700

    remove cookie_fix middleware and helper
    This will have to be rewritten somewhat to work with oauth2. Just trashing it for now.

--- lib/eukaliptus/middleware.rb
+++ lib/eukaliptus/middleware.rb
@@ -18,49 +18,8 @@ module Eukaliptus
 
       # Fixes IE security bug
       @response.header["P3P"] = 'CP="HONK HONK! http://graeme.per.ly/p3p-policies-are-a-joke"'
-
-      if env['PATH_INFO'] == '/cookie_fix'
-        cookie_fix(env)
-      else
-        @response.finish
-      end
-    end
-
-    # Get POST params and process it to build up the cookie for FB
-    # authentication. Then prepare the response for redirection and
-    # breaks the request workflow setting the cookie at the same time.
-    # This way Safari and other browsers with extra iframe security
-    # gets the cookie set too.
-    def cookie_fix(env)
-      params = @request.params
       
-      if params['_session_id']
-        session = ActiveSupport::JSON.decode(params['_session_id'])
-
-        unless (@request.cookies['fbs_' + Facebook::APP_ID.to_s].present?)
-          session = session.map { |key, value| key.to_s + "=" + value.to_s }.join("&")
-          @response.set_cookie('fbs_' + Facebook::APP_ID.to_s, session)
-        end
-      end
-
-      @response.headers.delete "Content-Type"
-      @response.headers.delete "Content-Length"
-      @response.headers.delete "X-Cascade"
-
-      if defined?(OmniAuth) and defined?(Devise)
-        mappings = Devise.mappings[:user]
-
-        if mappings.controllers.has_key? :omniauth_callbacks
-          path = [mappings.path, 'auth', :facebook.to_s, 'callback'].join('/')
-          @response.redirect(path + "?#{params['redirect_to'].to_query('redirect_to')}")
-        else
-          @response.redirect("/?#{params['redirect_to'].to_query('redirect_to')}")
-        end
-      else
-        @response.redirect((params['redirect_to'] ? params['redirect_to'] : '/'))
-      end
-
-      [302, @response.headers, ['Cookie Setted']]
+      @response.finish
     end
   end
 end
--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -11,21 +11,4 @@ class MiddlewareTest < MiniTest::Unit::TestCase
     get '/cookie_fix'
     assert_equal last_response["P3P"], 'CP="HONK HONK! http://graeme.per.ly/p3p-policies-are-a-joke"'
   end
-  
-  def test_should_redirect
-    get '/cookie_fix'
-    assert last_response.redirect?
-  end
-
-  def test_should_redirect_to_param
-    get '/cookie_fix', :redirect_to => '/somewhere'
-    assert_equal last_response['Location'], '/somewhere'
-  end
-
-  def test_should_respond_with_cookie
-    get '/cookie_fix', :redirect_to => '/'
-
-    assert_instance_of String, last_response.body
-    assert_equal 'Cookie Setted', last_response.body
-  end
 end
