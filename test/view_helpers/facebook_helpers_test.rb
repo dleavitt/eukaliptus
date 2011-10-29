d0a5d48349470ba90ffd192e7d0b062b3256581c
Daniel Leavitt <daniel.leavitt@gmail.com>
2011-10-29 01:06:12 -0700

    killed most of the JS helpers

--- test/view_helpers/facebook_helpers_test.rb
+++ test/view_helpers/facebook_helpers_test.rb
@@ -10,12 +10,6 @@ class FacebookHelpersTest < MiniTest::Spec
 		require 'active_support/core_ext/string/output_safety'
 	end
 
-	it "should_raise_exception_with_fb_login" do
-		assert_raises RuntimeError do
-			fb_login
-		end
-	end
-
 	it "should_return_fb_init_js" do
 		assert_instance_of ActiveSupport::SafeBuffer, fb_init
 	end
